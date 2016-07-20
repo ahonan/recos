d86faaf44c1083b3e72cb46eba1c8c10602cc6d7
ahonan <ajhonan26@gmail.com>
2016-07-20 18:45:47 -0500

    Update site.js

--- site.js
+++ site.js
@@ -2,35 +2,23 @@
 var main=function(){
 $('#menu-icon').click(function(){
     if($('.nav').hasClass('open')){
-        $('.nav').removeClass('open').animate({
-            left:"-250px"
-        }, 200);
-        $('body').animate({
-            left:"0px"
-        }, 200);
+        $('.nav').removeClass('open').animate({left:"-250px"}, 500);
+        $('body').animate({left:"0px"}, 500);
     }else{
-        $('.nav').addClass('open').animate({
-            left:"0px"
-        }, 200);
-        $('body').animate({
-            left:"250px"
-        }, 200);
+        $('.nav').addClass('open').animate({left:"0px"}, 500);
+        $('body').animate({left:"250px"}, 500);
     }
 });
 $('.dropdown').click(function(){
     if($('.sub-nav').hasClass('o')){
-        $('.sub-nav').toggle(false).toggleClass('o', false);
+        $('.sub-nav').toggle(400).removeClass('o');
+        $('#caret').addClass('caret-down').removeClass('caret-up');
     }else{
-        $('.sub-nav').toggle(true).toggleClass('o', true);
-    }
-    if($('.sub-nav').hasClass('o')){
-        $('.caret-up').css("display", "inline-block");
-        $('.caret-down').css("display", "none");
-    }else{
-        $('.caret-up').css("display", "none");
-        $('.caret-down').css("display", "inline-block");
+        $('.sub-nav').toggle(400).addClass('o');
+        $('#caret').addClass('caret-up').removeClass('caret-down');
     }
 });
 };
+$(document).ready(main);
 
 $(document).ready(main);
